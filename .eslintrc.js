9c89f1423e4435b5975231dafed469df298782c2
weberLong <weihua_long@qingsongchou.com>
2018-03-13 00:54:16 +0800

    add eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
+    'plugin:vue/essential',
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
     'standard'
   ],
@@ -21,8 +21,10 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
+    'arrow-parens': 0,
     // allow async-await
     'generator-star-spacing': 'off',
+    'no-tabs': 'off',
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
